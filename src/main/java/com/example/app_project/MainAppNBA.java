df6c4cfb26948efb5eb25343bf26ad420314d74d
Szymon Gawel <416522@student.agh.edu.pl>
2024-01-03 01:45:49 +0100

    startup gui

--- src/main/java/com/example/app_project/MainAppNBA.java
+++ src/main/java/com/example/app_project/MainAppNBA.java
@@ -8,17 +8,31 @@ import javafx.stage.Stage;
 
 public class MainAppNBA extends Application {
 
+    private Scene scene;
+
+
     @Override
     public void start(Stage primaryStage) {
         TeamSelection teamSelection = new TeamSelection();
 
         primaryStage.setTitle("NBA Team Selection");
-        primaryStage.setScene(new Scene(teamSelection.getLayout(), 300, 200));
+        scene = new Scene(teamSelection.getLayout(), 1600, 800);
+        primaryStage.setScene(scene);
         primaryStage.show();        //linie te opowiadają za ustawienie i wyświetlenie okna mojej aplikacji
                                     //natomiast cała logika przypisana do tego okna znajduje sie w innych klasach
+        primaryStage.setResizable(false);
+
+        scene.getStylesheets().add(getClass().getResource("/styles.css").toExternalForm());
+
+
+    }
+
+    public Scene getScene() {
+        return scene;
     }
 
     public static void main(String[] args) {
         launch(args);
     }
 }
+
